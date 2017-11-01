5c907ec599c67db724e77ad0cbf2cd54e298fe88
Kanogporn Nonsrichai <kanogporn.nonsrichai@rail.bombardier.com>
2017-11-01 14:44:27 +0700

    Remove unnecessary comments

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const cors=require('cors');
 
-// Get our API routes
+// Get  API routes
 const api = require('./server/api/routes/index');
 
 
@@ -15,31 +15,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Point static path to dist
-//app.use(express.static(path.join(__dirname, 'dist')));
+
 
 app.use(cors());
 
-// Set our api routes
+// Set api route
 app.use('/', api);
 
-// Catch all other routes and return the index file
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist/index.html'));
-// });
 
-/**
- * Get port from environment and store in Express.
- */
 const port = process.env.PORT || '3000';
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
+// Create HTTP server.
+
 const server = http.createServer(app);
 
-/**
- * Listen on provided port, on all network interfaces.
- */
 server.listen(port, () => console.log(`API running on localhost:${port}`));
